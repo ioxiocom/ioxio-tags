d5552b4312b46172da421b29ae72b329599a45ab
Janne Enberg <janne.enberg@lietu.net>
2023-09-08 15:22:50 +0300

    Some work towards a sensible starting point, no COSE verification still, seems impossible in JS

--- scanner/src/routes/+page.svelte
+++ scanner/src/routes/+page.svelte
@@ -15,6 +15,31 @@
     "EKBRBNJO3ATK1WEW+P-PL.R0%MQJVTL68+FMLOFRAJ0IHR6W882.8B:EBEG24X74H9COJ:.1C89WFCL M+.C1XJ$Z3PM5OM754GL22D7TVXV*JH" +
     "/X1U66+CHCEMXI0P274GSM.6. LBDD"
 
+  const PRESET_JWKS = {
+    keys: [
+      {
+        alg: "RS256",
+        kid: "01",
+        kty: "RSA",
+        n:
+          "yicAxwnv46tR82buQHN2b2dDLOmZ0iJYR6GPwNOShlCdN-N5N6spvAbe3UNb6ymN8AtW5o9_6Rm6gpungvlnDtjyZfPnNzAryjR8Z1" +
+          "Jvb33spr7zcLWG8TzL1l5r5b2zMnWXaDOZffAxa3SWZ14ZufAAA97fW3oBJXYr4H0zB8aFNuPDtYw4puN4s3M1vQH6OPgXDkSNYBv5Zbx" +
+          "lKasPXdFf_fXA9u4p9JMmtNS3fL7RibMqbrf4jCkATwVqA_Ui9vlgudZeHqW0tpzuXT3FVyVN-eFQwq6Hk3vY8iqRD9Skbpol9vHQLKY8" +
+          "px5tZZTxVtNcLhTKzNToMWQMykuq0Q",
+        e: "AQAB",
+      },
+    ],
+  }
+
+  type RawSecureTagParseResult = {
+    kid: string // JWKS key ID
+    payload: {
+      iss: string // Issuer domain
+      product: string // Generic product name / category
+      id: string // Unique ID of the product
+    }
+  }
+
   // TODO: On mobile devices where permissions allow for it, we should directly start scanning
   async function startScan() {
     // Check camera permission
@@ -69,13 +94,31 @@
         const b45decoded = decodeBase45(contents)
         const cborData = await parseCoseInsecure(b45decoded)
         console.log(cborData)
-        if (cborData.iss && cborData.product && cborData.id) {
+        if (
+          cborData.kid &&
+          cborData.payload.iss &&
+          cborData.payload.product &&
+          cborData.payload.id
+        ) {
           // This is an IOXIO Tags QR code
           console.log("IOXIO Tag detected, should continue to do more things")
 
           // TODO: Fetch metadata + JWKS keys from cborData.iss
           // TODO: Verify COSE signature with JWKS key
           // TODO: Display available data products based on metadata
+
+          // Instead right now just trying against preset key
+          const jwk = PRESET_JWKS.keys.find((key) => {
+            return key.kid === cborData.kid
+          })
+
+          let verified = false
+
+          if (!jwk) {
+            console.error("Couldn't find the JWKS key to verify this code")
+          } else {
+            verified = await verifyCose(b45decoded, jwk)
+          }
 
           return true
         }
@@ -85,6 +128,7 @@
           console.warn(`Not a valid IOXIO Tag: ${contents}`)
         } else {
           // TODO: Show error
+          console.error(e)
           return false
         }
       }
@@ -95,18 +139,39 @@
     return Buffer.from(value)
   }
 
-  async function parseCoseInsecure(message: string) {
+  function Uint8ArrayToString(value: Uint8Array): string {
+    return new TextDecoder().decode(value)
+  }
+
+  async function parseCoseInsecure(message: string): Promise<RawSecureTagParseResult> {
     // No verification of signature performed here
     const messageBuffer = stringToBuffer(message)
 
     const coseContainer = decodeCbor(messageBuffer)
     const [_headers1, _headers2, cborPayload, _signature] = coseContainer.value
 
-    return decodeCbor(cborPayload)
+    console.log({ _headers1, _headers2, cborPayload, _signature })
+
+    // Extract key ID from headers
+    // https://www.iana.org/assignments/cose/cose.xhtml
+    const kid = Uint8ArrayToString(_headers2[4])
+
+    return {
+      kid: kid,
+      payload: decodeCbor(cborPayload),
+    }
   }
 
-  async function parseCose(message: string) {
+  async function verifyCose(message: string, jwk: object): Promise<boolean> {
+    console.log(`Verifying: ${message} with ${JSON.stringify(jwk)}`)
+    const messageBuffer = stringToBuffer(message)
+
+    // cose-js library uses `require` etc. and doesn't seem to actually work in a browser for multiple reasons
+    // @mattrglobal/cose might work but their licensing is not compatible with .. anything
+    // might just have to submit this to a backend, maybe running Node, maybe running Python
     throw new Error("Cannot verify COSE yet")
+
+    return true
   }
 </script>
 
