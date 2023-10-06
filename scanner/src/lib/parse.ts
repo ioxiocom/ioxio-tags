6b411c49ef0664dbe3fa8096c683f6d11b3ba231
Fernando <fernandoquevedo2198@gmail.com>
2023-10-05 20:22:54 -0500

    removed unnecessary code

--- scanner/src/lib/parse.ts
+++ scanner/src/lib/parse.ts
@@ -10,22 +10,6 @@ if (typeof window !== "undefined") {
   }
 }
 
-// const PRESET_JWKS = {
-//   keys: [
-//     {
-//       alg: "RS256",
-//       kid: "01",
-//       kty: "RSA",
-//       n:
-//         "yicAxwnv46tR82buQHN2b2dDLOmZ0iJYR6GPwNOShlCdN-N5N6spvAbe3UNb6ymN8AtW5o9_6Rm6gpungvlnDtjyZfPnNzAryjR8Z1" +
-//         "Jvb33spr7zcLWG8TzL1l5r5b2zMnWXaDOZffAxa3SWZ14ZufAAA97fW3oBJXYr4H0zB8aFNuPDtYw4puN4s3M1vQH6OPgXDkSNYBv5Zbx" +
-//         "lKasPXdFf_fXA9u4p9JMmtNS3fL7RibMqbrf4jCkATwVqA_Ui9vlgudZeHqW0tpzuXT3FVyVN-eFQwq6Hk3vY8iqRD9Skbpol9vHQLKY8" +
-//         "px5tZZTxVtNcLhTKzNToMWQMykuq0Q",
-//       e: "AQAB",
-//     },
-//   ],
-// }
-
 const IOXIO_TAGS_VERSION_PREFIX = "IT1:"
 
 type Payload = {
@@ -69,18 +53,6 @@ async function parseCoseInsecure(message: Buffer): Promise<RawSecureTagParseResu
   }
 }
 
-// async function verifyCose(message: Buffer, jwk: object): Promise<boolean> {
-//   consoleLog(`Verifying: ${message} with ${JSON.stringify(jwk)}`)
-
-//   // cose-js library uses `require` etc. and doesn't seem to actually work in a browser for multiple reasons
-//   // @mattrglobal/cose might work but their licensing is not compatible with .. anything
-//   // might just have to submit this to a backend, maybe running Node, maybe running Python
-//   consoleLog("Cannot verify COSE yet")
-//   throw new Error("Cannot verify COSE yet")
-
-//   return true
-// }
-
 export async function tryParseIoxioTags(contents: string): Promise<Payload | undefined> {
   const isValidURL = isTagsURL(contents)
   if (isValidURL) {
@@ -109,22 +81,6 @@ export async function tryParseIoxioTags(contents: string): Promise<Payload | und
           // TODO: Fetch metadata + JWKS keys from cborData.iss
           // TODO: Verify COSE signature with JWKS key
           // TODO: Display available data products based on metadata
-
-          // Instead right now just trying against preset key
-          // const jwk = PRESET_JWKS.keys.find((key) => {
-          //   return key.kid === cborData.kid
-          // })
-
-          // let verified = false
-
-          // if (!jwk) {
-          //   consoleLog("Couldn't find the JWKS key to verify this code", "warn")
-          // } else {
-          //   verified = await verifyCose(b45decoded, jwk)
-          //   if (verified) {
-          //     consoleLog("Signature verified")
-          //   }
-          // }
 
           return cborData.payload
         }
