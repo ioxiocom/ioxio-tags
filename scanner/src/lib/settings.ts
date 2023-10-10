790c513420748bd9e3bdde8191963a95d1c0f829
Fernando <fernandoquevedo2198@gmail.com>
2023-10-10 09:06:53 -0500

    updated documentation

--- scanner/src/lib/settings.ts
+++ scanner/src/lib/settings.ts
@@ -3,5 +3,5 @@ import { env } from "$env/dynamic/public"
 export const settings = {
   DEVELOPMENT: env.PUBLIC_DEVELOPMENT === "true",
   PUBLIC_API_BASE_URL: env.PUBLIC_API_BASE_URL || "http://localhost:8081",
-  DOCUMENTATION_URL: "/documentation",
+  DOCUMENTATION_URL: "https://docs.ioxio.dev/tags/",
 }
--- scanner/src/routes/q/[iss]/[product]/[id]/+page.server.ts
+++ scanner/src/routes/q/[iss]/[product]/[id]/+page.server.ts
@@ -1,6 +1,6 @@
 /** @type {import('./$types').EntryGenerator} */
 export function entries() {
-  return [{ iss: "tags.ioxio.dev", product: "test", id: "test" }]
+  return [{ iss: "ioxio.com", product: "battery-100wh-super-turbo", id: "177389-09633" }]
 }
 
 export const prerender = true
