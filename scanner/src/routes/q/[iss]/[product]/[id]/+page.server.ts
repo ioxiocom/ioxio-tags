4905c42c9e95cb807ddfce3b965d2cdff6a79f9b
Janne Enberg <janne.enberg@lietu.net>
2023-10-10 18:05:51 +0300

    Fixed routing for `/q/`

--- scanner/src/routes/q/[iss]/[product]/[id]/+page.server.ts
+++ scanner/src/routes/q/[iss]/[product]/[id]/+page.server.ts
@@ -1,6 +1,1 @@
-/** @type {import('./$types').EntryGenerator} */
-export function entries() {
-  return [{ iss: "ioxio.com", product: "battery-100wh-super-turbo", id: "177389-09633" }]
-}
-
-export const prerender = true
+export const prerender = false
