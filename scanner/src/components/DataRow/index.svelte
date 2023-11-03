a00907d56e1c206230048fa02687589f71c967a0
Fernando <fernandoquevedo2198@gmail.com>
2023-11-03 17:44:39 -0500

    Implement food artifact nutritional values data product

---
+++ scanner/src/components/DataRow/index.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  export let label: string
+  export let value: string
+</script>
+
+<div class="row">
+  <div class="label">
+    {label}:
+  </div>
+  <div class="value">
+    {value}
+  </div>
+</div>
+
+<style lang="scss">
+  .row {
+    display: flex;
+    flex-direction: row;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 400;
+    &:not(:last-child) {
+      margin-bottom: 1rem;
+    }
+  }
+  .label {
+    flex: 0 0 45%;
+    padding-right: 0.5rem;
+  }
+  .value {
+    flex: 0 0 55%;
+  }
+</style>
