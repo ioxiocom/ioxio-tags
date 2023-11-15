5f581d8d72738f1deeb2efd1be04cb645eb1e198
Fernando <fernandoquevedo2198@gmail.com>
2023-11-15 16:02:05 -0500

    verification failed when scanning QR codes generated with generator

--- generator/src/lib/premadeProducts.ts
+++ generator/src/lib/premadeProducts.ts
@@ -6,14 +6,9 @@ export type Option = {
 
 export const premadeProducts = [
   {
-    name: "Battery - Valmet Automotive MPP48V (MPP48V-296cde7f)",
-    product: "MPP48V",
+    name: "Really Good Battery",
+    product: "battery-100wh-super-turbo",
     id: "MPP48V-296cde7f",
-  },
-  {
-    name: "Battery - FZSONICK FZSBTR (FZSBTR-Z5-278-76)",
-    product: "FZSONICK battery",
-    id: "FZSBTR-Z5-278-76",
   },
   {
     name: "Oolannin Uunirapeat Ranskanperunat 500g (4750976000)",
