6a71a31a55cd3c83707e096b4d067a52dbbb2a13
Janne Enberg <janne.enberg@lietu.net>
2024-01-24 14:45:07 +0200

    Removing unimplemented demo data

--- generator/src/lib/premadeProducts.ts
+++ generator/src/lib/premadeProducts.ts
@@ -6,16 +6,6 @@ export type Option = {
 
 export const premadeProducts = [
   {
-    name: "Battery - Valmet Automotive MPP48V (MPP48V-296cde7f)",
-    product: "MPP48V",
-    id: "MPP48V-296cde7f",
-  },
-  {
-    name: "Battery - FZSONICK FZSBTR (FZSBTR-Z5-278-76)",
-    product: "FZSONICK battery",
-    id: "FZSBTR-Z5-278-76",
-  },
-  {
     name: "Oolannin Uunirapeat Ranskanperunat 500g (4750976000)",
     product: "uunirapeat-ranskanperunat-500g-121xco3213",
     id: "4750976000",
