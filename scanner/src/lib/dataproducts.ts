146d3f5dc03bbf0ecdc4926be52750e81bc75512
ThElias <akande.tolu@yahoo.com>
2024-01-30 11:12:33 +0100

    fix: file structure

--- scanner/src/lib/dataproducts.ts
+++ scanner/src/lib/dataproducts.ts
@@ -1,21 +1,21 @@
 import FoodArtifact_NutritionalValues_v0_1 from "$lib/dataproducts/FoodArtifact_NutritionalValues_v0_1.svelte"
 import MetalArtifact_DataSheet_v0_1 from "$lib/dataproducts/MetalArtifact_DataSheet_v0_1.svelte"
 import LogisticsEmissions_v0_1 from "$lib/dataproducts/LogisticsEmissions_v0_1.svelte"
-import CarbonFootprint_v0_1 from "$lib/dataproducts/battery/CarbonFootprint_v0_1.svelte"
-import BatteryManufacturingDataSheet_v0_1 from "$lib/dataproducts/battery/ManufacturingDataSheet_v0.1.svelte"
-import HealthData_v0_1 from "$lib/dataproducts/battery/HealthData_v0.1.svelte"
-import EnvironmentalFootprint_v0_1 from "$lib/mobile-work-machine/EnvironmentalFootprint_v0.1.svelte"
-import DrillManufacturingDataSheet_v0_1 from "$lib/mobile-work-machine/DrillManufacturingDataSheet_v0.1.svelte"
+import Battery_CarbonFootprint_v0_1 from "$lib/dataproducts/Battery_CarbonFootprint_v0_1.svelte"
+import Battery_ManufacturingDataSheet_v0_1 from "$lib/dataproducts/Battery_ManufacturingDataSheet_v0.1.svelte"
+import Battery_HealthData_v0_1 from "$lib/dataproducts/Battery_HealthData_v0.1.svelte"
+import MobileWorkMachine_EnvironmentalFootprint_v0_1 from "$lib/dataproducts/MobileWorkMachine_EnvironmentalFootprint_v0.1.svelte"
+import MobileWorkMachine_Drill_ManufacturingDataSheet_v0_1 from "$lib/dataproducts/MobileWorkMachine_Drill_ManufacturingDataSheet_v0.1.svelte"
 
 export const supportedDataProducts = {
   "DigitalProductPassport/FoodArtifact/NutritionalValues_v0.1": FoodArtifact_NutritionalValues_v0_1,
   "DigitalProductPassport/MetalArtifact/DataSheet_v0.1": MetalArtifact_DataSheet_v0_1,
   "DigitalProductPassport/LogisticsEmissions_v0.1": LogisticsEmissions_v0_1,
-  "DigitalProductPassport/Battery/CarbonFootprint_v0.1": CarbonFootprint_v0_1,
-  "DigitalProductPassport/Battery/ManufacturingDataSheet_v0.1": BatteryManufacturingDataSheet_v0_1,
-  "DigitalProductPassport/Battery/HealthData_v0.1": HealthData_v0_1,
+  "DigitalProductPassport/Battery/CarbonFootprint_v0.1": Battery_CarbonFootprint_v0_1,
+  "DigitalProductPassport/Battery/ManufacturingDataSheet_v0.1": Battery_ManufacturingDataSheet_v0_1,
+  "DigitalProductPassport/Battery/HealthData_v0.1": Battery_HealthData_v0_1,
   "DigitalProductPassport/MobileWorkMachine/EnvironmentalFootprint_v0.1":
-    EnvironmentalFootprint_v0_1,
+    MobileWorkMachine_EnvironmentalFootprint_v0_1,
   "DigitalProductPassport/MobileWorkMachine/Drill/ManufacturingDataSheet_v0.1":
-    DrillManufacturingDataSheet_v0_1,
+    MobileWorkMachine_Drill_ManufacturingDataSheet_v0_1,
 }
