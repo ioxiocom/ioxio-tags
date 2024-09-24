1246a3135c51eb24db5b66303a652ad10456839d
fbjorn <denis@fbjorn.cc>
2024-09-24 15:00:43 +0300

    Add 2 more data definitions for a rock drill (#76)
    * Add 2 data definitions support

---
+++ scanner/src/lib/dataproducts/Machine_ComponentSerialNumbers_v0.1.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  /*
+      https://github.com/ioxio-dataspace/sandbox-definitions/blob/main/src/DigitalProductPassport/Machine/ComponentSerialNumbers_v0.1.py
+  */
+
+  import DataRow from "$components/DataRow/index.svelte"
+  import Article from "$components/Article/index.svelte"
+
+  export let data: {
+    serialNumbers: {
+      name: string
+      serial: string
+    }[]
+  }
+</script>
+
+<Article>
+  {#each data.serialNumbers as sn}
+    <DataRow label={sn.name} value={sn.serial} />
+  {/each}
+</Article>
